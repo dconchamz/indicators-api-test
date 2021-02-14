d15fbfa84ca26eb626b120ffd711b6010bd04396
Dancko <dancko.concha@azlo.com>
2021-02-14 02:54:56 -0300

    feat: indicators api test

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+// configure bodyparser and cors
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || '3000';
+
+routes(app);
+//http://localhost:3000?indicator=dolar
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port} a`);
+});
