c970a6106378e1401a3d3c24e2779065778e1964
Dancko <dancko.concha@azlo.com>
2021-02-14 02:19:36 -0300

    jaskj

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    'pre-push': 'yarn run code:check',
+    'pre-commit': 'yarn test --watchAll=false --passWithNoTests && yarn run lint-staged',
   },
 };
