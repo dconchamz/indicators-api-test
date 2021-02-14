c9cddf6d20eb408ed8b51b647b7468c92dd21dff
Dancko <dancko.concha@azlo.com>
2021-02-14 03:36:31 -0300

    feat: add comments to know the code why request

--- src/services/indicator.js
+++ src/services/indicator.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+// you can request by uf,ivp,dolar,euro,ipc, imacec, utm, tpm, libra_cobre,tasa_desempleo, bitcoin 
 function getIndicator(indicator) {
   return axios.get(`https://mindicador.cl/api/${indicator}`);
 }
