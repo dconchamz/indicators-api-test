a8af46a5e7726479802713bd5fbd49e843e4e70a
Dancko <dancko.concha@azlo.com>
2021-02-14 02:49:04 -0300

    feat: prueba commit lint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [2, "always", ["feat", "fix", "FEATURE"]],
-    "type-case": [0, "always", ["lower-case"]],
-  },
+  extends: ["@commitlint/config-conventional"]
 };
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const indicatorRoutes = require('./indicator');
-const apiV1 = '/v1/indicators';
-
-module.exports = (app) => {
-    app.use(`${apiV1}`, indicatorRoutes);
-};
-  
