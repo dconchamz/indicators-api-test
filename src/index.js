7ef1e3b36fd893a001cfa79b675bc2b4720537a3
Dancko <dancko.concha@azlo.com>
2021-02-14 03:31:28 -0300

    feat: test commitlint + husky

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ routes(app);
 //http://localhost:3000?indicator=dolar
 
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
+  console.log(`listening at http://localhost:${port} a`);
 });
