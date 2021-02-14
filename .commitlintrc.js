913b34782e746c48d9fc7f075b226b683da7e934
Dancko <dancko.concha@azlo.com>
2021-02-14 02:40:01 -0300

    kajkja

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "FEATURE"]],
+    "type-case": [0, "always", ["lower-case"]],
+  },
 };
