8b14dc68e51d1159e79b8977ab08b56d9591cec4
Dancko <dancko.concha@azlo.com>
2021-02-14 03:02:08 -0300

    feat: delete all

--- src/routes/indicator.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { indicators } = require('../controllers/indicator');
-
-router.get('/', indicators);
-
-module.exports = router;
--- src/services/indicator.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-function getIndicator(indicator) {
-  return axios.get(`https://mindicador.cl/api/${indicator}`);
-}
-
-module.exports = { getIndicator };
